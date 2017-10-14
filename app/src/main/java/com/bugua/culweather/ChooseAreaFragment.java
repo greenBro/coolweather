5ca7af3941fda50407905178b12da7de79a379a0
greenBro <942746436@qq.com>
2017-10-14 11:54:12 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/bugua/culweather/ChooseAreaFragment.java
@@ -0,0 +1,10 @@
+package com.bugua.culweather;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Administrator on 2017-10-13.
+ */
+
+public class ChooseAreaFragment extends Fragment {
+}
